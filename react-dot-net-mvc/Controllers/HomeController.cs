649217b53b24cf2cfdfd2a6ad7c4fca18580672a
sanjsanj <sanjsanj@hotmail.com>
2017-06-17 09:47:37 +0100

    Can load react components in .NET MVC4/5

---
+++ react-dot-net-mvc/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using react_dot_net_mvc.Models;
+using System.Web.UI;
+
+namespace react_dot_net_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [OutputCache(Location = OutputCacheLocation.None)]
+        public ActionResult Comments()
+        {
+            return Json(_comments, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ react-dot-net-mvc/Scripts/Tutorial.jsx
@@ -0,0 +1,49 @@
+﻿const data = [
+    { Id: 1, Author: "Daniel Lo Nigro", Text: "Hello ReactJS.NET World!" },
+    { Id: 2, Author: "Pete Hunt", Text: "This is one comment" },
+    { Id: 3, Author: "Jordan Walke", Text: "This is *another* comment" }
+]
+
+const Comment = ({ author, text }) => (
+    <div className="comment">
+        <h2 className="commentAuthor">
+            {author}
+        </h2>
+        {text}
+    </div>
+)
+
+const CommentList = ({ data }) => {
+    const comments = data.map(comment => (
+        <Comment
+            key={comment.Id}
+            author={comment.Author}
+            text={comment.Text}
+        />
+    ))
+
+    return (
+        <div className="commentList">
+            {comments}
+        </div>
+    )
+}
+
+const CommentForm = () => (
+    <div className="commentForm">
+        Hello, world! I am a CommentForm.
+    </div>
+)
+
+const CommentBox = ({ data }) => (
+    <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={data} />
+        <CommentForm />
+    </div>
+)
+
+ReactDOM.render(
+    <CommentBox data={data} />,
+    document.getElementById('content'),
+)
